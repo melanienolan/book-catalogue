2c78d285efca484b9adb722731c53280acb27f61
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2017-09-19 14:30:40 +0200

    conditionally render class names for selected button

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -3,36 +3,28 @@ import PropTypes from 'prop-types';
 import logo from '../logo.svg';
 
 const Menu = props => {
+  let booksOpen = props.route === 'books';
   return (
     <div className="menu--holder">
       <div className="logo--holder">
         <img src={logo} className="logo" alt="logo" />
       </div>
-      {props.route === 'books'
-        ? <nav className="menu">
-            <button
-              className="button button--tab button--tab-selected"
-              onClick={() => props.onMenuClick('books')}>
-              Books
-            </button>
-            <button
-              className="button button--tab"
-              onClick={() => props.onMenuClick('genres')}>
-              Genres
-            </button>
-          </nav>
-        : <nav className="menu">
-            <button
-              className="button button--tab"
-              onClick={() => props.onMenuClick('books')}>
-              Books
-            </button>
-            <button
-              className="button button--tab button--tab-selected"
-              onClick={() => props.onMenuClick('genres')}>
-              Genres
-            </button>
-          </nav>}
+      <nav className="menu">
+        <button
+          className={
+            booksOpen ? 'button button--tab button--tab-selected' : 'button button--tab'
+          }
+          onClick={() => props.onMenuClick('books')}>
+          Books
+        </button>
+        <button
+          className={
+            booksOpen ? 'button button--tab' : 'button button--tab button--tab-selected'
+          }
+          onClick={() => props.onMenuClick('genres')}>
+          Genres
+        </button>
+      </nav>
     </div>
   );
 };
